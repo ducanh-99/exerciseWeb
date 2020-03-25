170b8d9a5e8df7c6dc4c3568dc95a598f2e0b6ef
anhptvnist <anhpt.vnist@gmail.com>
2020-03-25 14:20:43 +0700

    Week 3

--- Week3t/lab4_exp1.php
+++ Week3t/lab4_exp1.php
@@ -1,15 +1,36 @@
 <!DOCTYPE html>
 <html>
-<body>
+<head>
+    <link type="text/css" rel="stylesheet" href="style3.css"/>
+</head>
 
-<form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="GET">
-    <select name="n" id="n">
-        <option>1</option>
-        <option>2</option>
-        <option>3</option>
-        <option>4</option>
-        <input type="submit" name="sm_get" value="Ready"/>
-    </select>
+<body>
+    <div class="container" style="margin: 0px 400px 0px 400px">
+        <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="GET">
+            <div class="center">
+                <h1>Upload File</h1>
+            </div>
+            <br/>
+        <div  style="font-size: 10px;">
+            <h2>Enter your number of files you want to upload</h1>
+        </div>
+        <br/>
+    <div class="row">
+        <label class="col-45" style="font-weight: bold;">Choose your number</label>
+        <div class="col-55">
+            <select style="float: left;"name="n" id="n">
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+                <option>5</option>
+                <option>6</option>
+            </select>
+            <input type="submit" style="width: 80px;" name="sm_get" value="Ready"/>
+            
+        </div>
+    </div>
+    
 </form>
 
 <?php
@@ -19,18 +40,27 @@ if ($_SERVER["REQUEST_METHOD"] == "GET") {
         ?>
         <form action="upload.php" enctype="multipart/form-data" method="POST">
             <?php
-            echo '<br><strong>You have allow to upload ' . "<input type=\"text\" name=\"n1\" id=\"n1\" readonly size=\"2\" value=\"$n\">" . ' file.</strong>';
+            echo '<br><strong>You have allow to upload ' . "<input type=\"text\"  name=\"n1\" id=\"n1\" readonly size=\"2\" value=\"$n\">" . ' file.</strong><br>';
+
             for ($i = 0; $i < $n; $i++) {
                 echo '<br><input name="fileToUpload' . $i . '" id="fileToUpload' . $i . '" type="file" ><br>';
             }
 
             ?>
-            <input type="submit" name="sm_post" value="Upload">
+            <br>
+            <div class="row">
+                <label class="col-45"></label>
+                <div class="col-55">
+                    <input type="submit" style="width: 80px;" name="sm_post" value="Upload">
+                </div>
+            </div>
         </form>
         <?php
     }
 }
 ?>
-
+<br>
+<br>
+    </div>
 </body>
 </html>
--- Week3t/upload.php
+++ Week3t/upload.php
@@ -21,56 +21,94 @@ for ($i = 0; $i < $max; $i++) {
         $uploadOk = 0;
     }
 }
-//upload thất bại
-if ($uploadOk == 0) {
-    exit('Sorry, your file was not uploaded."."<br>"');
-// if everything is ok, try to upload file
-} else for ($i = 0; $i < $max; $i++) {
-    if (move_uploaded_file($_FILES["fileToUpload" . $i]["tmp_name"], $target_file[$i])) {
-        echo "The file " . basename($_FILES["fileToUpload" . "$i"]["name"]) . " has been uploaded." . "<br>";
-    } else {
-        exit('Sorry, there was an error uploading your file');
-    }
-}
-//hiện thị tệp tin ra màn hình
-for ($i = 0; $i < $max; $i++) {
-    if ($imageFileType[$i] == "jpg" || $imageFileType[$i] == "png" || $imageFileType[$i] == "jpeg" || $imageFileType[$i] == "gif")
-        echo "<div id=\"image$i\" name=\"image$i\"><img src=" . $target_file_RG[$i] . "  width=\"100px\" height=\"100px\"></div>";
-    echo "<div id=\"name$i\" name=\"name$i\"><br>" . "Name: " . basename($_FILES["fileToUpload" . $i]["name"]) . "<br></div>";
-    echo "Type: " . basename($_FILES["fileToUpload" . $i]["type"]) . "<br>";
-    echo "Size: " . basename($_FILES["fileToUpload" . $i]["size"]) . "B <br>";
-    echo "tmp_type: " . basename($_FILES["fileToUpload" . $i]["tmp_name"]) . "<br>";
-    echo "last modified: " . date("F d Y H:i:s.",filemtime("uploadLab4/".basename($_FILES["fileToUpload" . $i]["name"]))) . "<br><br><br><br>";
-}
 ?>
-
-<?php
-//xử lý sắp xêp tệp tin
-echo "Sắp xếp theo tên:"."<br>";
-$f = array();
-$g = array();
-for ($i = 0; $i < $max; $i++) $f[$i] = basename($_FILES["fileToUpload" . $i]["name"]);
-for ($i = 0; $i < $max; $i++) $g[$i] = $i;
-for ($i = 0; $i < $max; $i++) {
-    for ($j = $i + 1; $j < $max; $j++) {
-        if ($f[$i] > $f[$j]) {
-            $tg = $f[$i];
-            $f[$i] = $f[$j];
-            $f[$j] = $tg;
-
-            $tg = $g[$i];
-            $g[$i] = $g[$j];
-            $g[$j] = $tg;
+<html>
+    <head>
+        <link type="text/css" rel="stylesheet" href="style3.css"/>
+    </head>
+    <body>
+    <div class="container" style="margin: 0px 200px 0px 200px">
+        <div class="center">
+            <h1>Upload file</h1>
+        </div>
+    <div style="padding-left: 20%;">
+        <?php
+        //upload thất bại
+        if ($uploadOk == 0) {
+            exit('Sorry, your file was not uploaded."."<br>"');
+        // if everything is ok, try to upload file
+        } else for ($i = 0; $i < $max; $i++) {
+            if (move_uploaded_file($_FILES["fileToUpload" . $i]["tmp_name"], $target_file[$i])) {
+                echo "The file " . basename($_FILES["fileToUpload" . "$i"]["name"]) . " has been uploaded." . "<br>";
+            } else {
+                exit('Sorry, there was an error uploading your file');
+            }
         }
-    }
-}
-for ($i = 0; $i < $max; $i++) {
-    if ($imageFileType[$g[$i]] == "jpg" || $imageFileType[$g[$i]] == "png" || $imageFileType[$g[$i]] == "jpeg" || $imageFileType[$g[$i]] == "gif")
-        echo "<img src=" . $target_file_RG[$g[$i]] . "  width=\"100px\" height=\"100px\">";
-    echo "<br>" . "Name: " . basename($_FILES["fileToUpload" . $g[$i]]["name"]) . "<br>";
-    echo "Type: " . basename($_FILES["fileToUpload" . $g[$i]]["type"]) . "<br>";
-    echo "Size: " . basename($_FILES["fileToUpload" . $g[$i]]["size"]) . "B <br>";
-    echo "tmp_type: " . basename($_FILES["fileToUpload" . $g[$i]]["tmp_name"]) . "<br>";
-    echo "last modified: " . date("F d Y H:i:s.",filemtime("uploadLab4/".basename($_FILES["fileToUpload" . $g[$i]]["name"]))) . "<br><br><br><br>";
-}
-?>
+        ?>
+
+       
+        <?php
+        for ($i = 0; $i < $max; $i++) {
+            if ($imageFileType[$i] == "jpg" || $imageFileType[$i] == "png" || $imageFileType[$i] == "jpeg" || $imageFileType[$i] == "gif")
+                echo "<div id=\"image$i\" name=\"image$i\"><img src=" . $target_file_RG[$i] . "  width=\"100px\" height=\"100px\"></div>";
+            echo "<div id=\"name$i\" name=\"name$i\"><br>" . "Name: " . basename($_FILES["fileToUpload" . $i]["name"]) . "<br></div>";
+            echo "Type: " . basename($_FILES["fileToUpload" . $i]["type"]) . "<br>";
+            echo "Size: " . basename($_FILES["fileToUpload" . $i]["size"]) . "B <br>";
+            echo "tmp_type: " . basename($_FILES["fileToUpload" . $i]["tmp_name"]) . "<br>";
+            echo "last modified: " . date("F d Y H:i:s.",filemtime("uploadLab4/".basename($_FILES["fileToUpload" . $i]["name"]))) . "<br><br><br><br>";
+        }
+        ?>
+    </div>
+        <hr>
+        <div class="center">
+            <h1>Sắp xếp theo tên</h1>
+        </div>
+        <?php
+        //xử lý sắp xêp tệp tin
+        //sắp xếp theo tên
+        $f = array();
+        $g = array();
+        for ($i = 0; $i < $max; $i++) $f[$i] = basename($_FILES["fileToUpload" . $i]["name"]);
+        for ($i = 0; $i < $max; $i++) $g[$i] = $i;
+        for ($i = 0; $i < $max; $i++) {
+            for ($j = $i + 1; $j < $max; $j++) {
+                if ($f[$i] > $f[$j]) {
+                    $tg = $f[$i];
+                    $f[$i] = $f[$j];
+                    $f[$j] = $tg;
+
+                    $tg = $g[$i];
+                    $g[$i] = $g[$j];
+                    $g[$j] = $tg;
+                }
+            }
+        }
+        ?>
+        <table border="1">
+            <tr>
+                <td style="width:15%; text-align: center;">Name</td> 
+                <td style="width:20%; text-align: center;">Type</td>
+                <td style="width:15%; text-align: center;">Size</td>
+                <td style="width:15%; text-align: center;">Tmp_type</td>
+                <td style="width:15%; text-align: center;">Last modified</td>
+                <td style="width:100px; text-align: center;">image</td>
+            </tr>
+            <?php
+            for ($i = 0; $i < $max; $i++) {
+                echo'<tr>';
+                echo '<td style="text-align: center;">' . basename($_FILES["fileToUpload" . $g[$i]]["name"]) . '</td>';
+                echo '<td style="text-align: center;">' . basename($_FILES["fileToUpload" . $g[$i]]["type"]) . "</td>";
+                echo '<td style="text-align: center;">' . basename($_FILES["fileToUpload" . $g[$i]]["size"]) . "B </td>";
+                echo '<td style="text-align: center;">' . basename($_FILES["fileToUpload" . $g[$i]]["tmp_name"]) . "</td>";
+                echo '<td style="text-align: center;">' . date("F d Y H:i:s.",filemtime("uploadLab4/".basename($_FILES["fileToUpload" . $g[$i]]["name"]))) . "</td>";
+                if ($imageFileType[$g[$i]] == "jpg" || $imageFileType[$g[$i]] == "png" || $imageFileType[$g[$i]] == "jpeg" || $imageFileType[$g[$i]] == "gif")
+                    echo "<td text-align: center;><img src=" . $target_file_RG[$g[$i]] . "  width=\"100px\" height=\"100px\"></td>";
+                
+                ?>
+                </tr>
+            <?php } ?>
+        </table>
+    </div>
+</div>
+</body>
+</html>
